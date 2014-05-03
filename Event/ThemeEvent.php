7967ef2b09ab088df0b121be778023068c9f021a
Marc Bach <mail@avanzu.de>
2014-05-03 09:25:53 +0200

    apidoc update

--- Event/ThemeEvent.php
+++ Event/ThemeEvent.php
@@ -9,6 +9,10 @@ namespace Avanzu\AdminThemeBundle\Event;
 
 use Symfony\Component\EventDispatcher\Event;
 
+/**
+ * Base event class to make theme related events easier to detect
+ *
+ */
 class ThemeEvent extends Event {
 
 }
--- Model/MessageInterface.php
+++ Model/MessageInterface.php
@@ -8,9 +8,35 @@
 namespace Avanzu\AdminThemeBundle\Model;
 
 
+/**
+ * Representation of a displayable message in the theme's messages section
+ *
+ */
 interface MessageInterface {
+    /**
+     * Returns the sender
+     *
+     * @return mixed
+     */
     public function getFrom();
+
+    /**
+     * Returns the sentAt date
+     * @return \DateTime
+     */
     public function getSentAt();
+
+    /**
+     * Returns the subject
+     *
+     * @return string
+     */
     public function getSubject();
+
+    /**
+     * Returns the unique identifier of this message
+     *
+     * @return mixed
+     */
     public function getIdentifier();
 }
