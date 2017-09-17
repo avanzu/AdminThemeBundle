bae4b5f2d147c5fec3a69f459254bc416b1a4e68
Ángel Guzmán Maeso <shakaran@gmail.com>
2017-09-17 08:27:15 +0200

    Fix extension name in yaml (yml)

--- DependencyInjection/AvanzuAdminThemeExtension.php
+++ DependencyInjection/AvanzuAdminThemeExtension.php
@@ -34,7 +34,7 @@ class AvanzuAdminThemeExtension extends Extension implements PrependExtensionInt
         catch(FileLocatorFileNotFoundException $e) // Symfony 3.3 and 4.x are based in YAML
         {
             $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-            $loader->load('services.yaml');
+            $loader->load('services.yml');
         }
     }
 
