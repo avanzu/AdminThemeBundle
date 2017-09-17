1df2333b88ec6de3ba82bbafbd298f84c2ad706a
Ángel Guzmán Maeso <shakaran@gmail.com>
2017-09-17 08:56:51 +0200

    Add default param options

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -30,6 +30,9 @@ class Configuration implements ConfigurationInterface
                     ->scalarNode('use_twig')
                         ->defaultValue(true)
                     ->end()
+                    ->scalarNode('options')
+                        ->defaultValue(NULL)
+                    ->end()
                 ->end();
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
