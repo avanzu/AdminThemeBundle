83004d940e649777b03357ecaed635d14ee2026f
Ángel Guzmán Maeso <shakaran@gmail.com>
2019-09-26 03:30:48 +0200

    <bugfix> Avoid conflict names with Command class

--- Command/CompactVendorCommand.php
+++ Command/CompactVendorCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Shell\Command;
+use Symfony\Component\Finder\Shell\Command as FCommand;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Process\Process;
 
